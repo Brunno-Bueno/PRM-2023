3e077f407834c34cad8efa48b943adcab51b043a
Bruno <crimatou364@gmail.com>
2023-08-14 19:44:04 -0300

    feat: Implementação do Skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import TopicCard from "./components/TopicCard";
 import {Divider}from '@mui/material';
+import TopicCardSkeleton from "./components/TopicCardSkeleton";
 function App() {
 
   return (
     <div id="App">
-      <TopicCard />
+        <TopicCard />
+        <Divider />
+        <TopicCard />
         <Divider />
         <TopicCard />
         <Divider />
         <TopicCard />
         <Divider />
+        <TopicCardSkeleton />
     </div>
   )
 }
