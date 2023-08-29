709e21948d0a46996ece0147455ac27ebe3f94ad
Bruno <crimatou364@gmail.com>
2023-08-29 19:08:29 -0300

    Aulas 08/08/2023 e 15/08/2023

--- frontend/src/components/HeaderProfile/index.tsx
+++ frontend/src/components/HeaderProfile/index.tsx
@@ -1,44 +1,55 @@
 import { Avatar, Box, Typography } from "@mui/material";
+import { CalendarMonthOutlined } from "@mui/icons-material";
+
+import { IntlProvider, FormattedDate } from 'react-intl';
+
+//Import de Imagens
 import banner from '../../assets/img/profile_banner.png';
-import avatar from '../../assets/img/jair-bolsonaro.png';
-import {CalendarMonthOutlined} from "@mui/icons-material";
+import avatar from '../../assets/img/profile_avatar.jpg';
+
+//Import do Estilo
 import './style.css'
 
-function HeaderProfile(){
+type HeaderProfileProps = {
+    user: any
+}
+
+function HeaderProfile({
+    user
+}: HeaderProfileProps) {
     return (
         <Box id="header-profile">
 
             <Box className="header-profile-background">
                 <img src={banner} />
             </Box>
-            
-            
+
             <Box className="header-profile-detail">
-                <Avatar alt="Fulano de Tal" style={{width: 128, height:128}} 
-                        src={avatar} className="header-profile-detail-avatar"/>
+                <Avatar alt={ user.fullname } style={{width: 128, height: 128}}
+                        src={avatar} className="header-profile-detail-avatar" />
+                
+                <Box className="header-profile-detail-text">                    
+                    <Typography variant="h5">
+                        { user.fullname }
+                    </Typography>
 
-            <Box className="header-profile-detail-text">
-                <Typography variant="h5">
-                    Fulano de Tal
-                </Typography>
+                    <Typography variant="subtitle1" component="h6">
+                        @{ user.username }
+                    </Typography>
 
-                <Typography variant="subtitle1" component="h6">
-                    @FualnoDeTal
-                </Typography>
+                    <Typography variant="subtitle1" component="p">
+                        { user.description }
+                    </Typography>
 
-                <Typography variant="subtitle1" component="p">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia 
-                    adipisci eos fugiat cupiditate quidem debitis, necessitatibus fuga reiciendis at ullam blanditiis accusamus architecto laudantium, 
-                    cumque culpa quam aperiam eligendi ducimus!
-                </Typography>
-
-                <Typography variant="caption" component="span">
-                    <CalendarMonthOutlined />
-                    Entrou em agosto de 2023
-                </Typography>
+                    <Typography variant="caption">
+                        <CalendarMonthOutlined />
+                        <IntlProvider locale="pt-BR">
+                            Entrou em <FormattedDate value={ user.createdAt } month="long" year="numeric" />
+                        </IntlProvider>
+                        
+                    </Typography>
+                </Box>
             </Box>
-            </Box>
-
 
         </Box>
     )
--- frontend/src/components/TopicCard/index.tsx
+++ frontend/src/components/TopicCard/index.tsx
@@ -1,17 +1,20 @@
-
-import TopicCardHeader from "../TopicCardHeader";
-import TopicCardBody from "../TopicCardBody";
 import TopicCardActions from "../TopicCardActions";
+import TopicCardBody from "../TopicCardBody";
+import TopicCardHeader from "../TopicCardHeader";
 
-function TopicCard(){
+type TopicCardProps = {
+    topic: any
+}
+
+function TopicCard({
+    topic
+}: TopicCardProps) {
     return (
-
         <div id="topic-card">
-            <TopicCardHeader/>
-            <TopicCardBody/>
-            <TopicCardActions/>
+            <TopicCardHeader />
+            <TopicCardBody />
+            <TopicCardActions />
         </div>
-
     )
 }
 
